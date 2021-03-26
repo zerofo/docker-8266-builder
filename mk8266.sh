90da2a5407a27fb05324dc39467b48bf03bddc75
Your Name <you@example.com>
2021-03-26 09:47:07 +0800

    增加提示

--- mk8266.sh
+++ mk8266.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 packuptime=`date '+%Y-%m-%d_%H%M%S'`
 if [[ ! -f /config.txt ]]; then
-    echo "没有找到config.txt";
+    echo "";
+    echo -e "\033[43;31m ############################# \033[0m";
+    echo -e "\033[43;31m #     没有找到config.txt    # \033[0m";
+    echo -e "\033[43;31m ############################# \033[0m";
+    echo "";
     exit;
 fi;
 if [[ ! -d /mk8266 || -z `ls /mk8266` ]]; then
-    echo "没有找到 需要打包的项目";
+    echo "";
+    echo -e "\033[43;31m ############################# \033[0m";
+    echo -e "\033[43;31m #  没有找到 需要打包的项目  # \033[0m";
+    echo -e "\033[43;31m ############################# \033[0m";
+    echo "";
     exit;
 fi;
 for f in `find /mk8266/ -type f -name *.manifest`;
@@ -14,28 +22,43 @@ do
 done;
 
 if [[ -z $cacheF ]]; then
-    echo "该网站没有找到manifest格式的离线缓存文件";
     echo "";
-    echo "请 修改对应的 cache/appcache 后缀的文件";
+    echo -e "\033[43;31m ########################################## \033[0m";
+    echo -e "\033[43;31m #该网站没有找到manifest格式的离线缓存文件# \033[0m";
+    echo -e "\033[43;31m #                                        # \033[0m";
+    echo -e "\033[43;31m # 请修改对应的 cache/appcache 后缀的文件 # \033[0m";
+    echo -e "\033[43;31m #                                        # \033[0m";
+    echo -e "\033[43;31m #  与之对应的 html 文件 第一二行 文件名  # \033[0m";
+    echo -e "\033[43;31m #                                        # \033[0m";
+    echo -e "\033[43;31m #              修改后重试                # \033[0m";
+    echo -e "\033[43;31m #                                        # \033[0m";
+    echo -e "\033[43;31m #        该源码包  离线缓存不符合，      # \033[0m";
+    echo -e "\033[43;31m #         请选择，否则10秒后退出：       # \033[0m";
+    echo -e "\033[43;31m #                                        # \033[0m";
+    echo -e "\033[43;31m #      ****强制自动替换  请输入(I)       # \033[0m"; 
+    echo -e "\033[43;31m # (可能无效,适合cache/appcache格式的缓存)# \033[0m";
+    echo -e "\033[43;31m #                                        # \033[0m";
+    echo -e "\033[43;31m #      ****强制忽略 请输入(Y)            # \033[0m";
+    echo -e "\033[43;31m # (可能无效,适合没有离线缓存/不检测缓存) # \033[0m";
+    echo -e "\033[43;31m ########################################## \033[0m";
     echo "";
-    echo " 与之对应的 html 文件 第一二行 文件名";
-    echo "";
-    echo " 修改后从试";
-    echo "";
-    echo " 该源码包  离线缓存不符合， 请选择，否则10秒后退出：";
-    echo "";
-    echo "强制自动替换(可能无法使用,适合使用cache/appcache格式缓存的离线包) 请输入(I)"
-    echo "";
-    echo "强制忽略(可能无法使用,适合默认没有离线缓存，不检测离线缓存的离线包) 请输入(Y)"
     read -t 10 -n 1 Icache
     echo "";
     if [[ $Icache == "Y" || $Icache == "y" ]]
     then 
-        echo "强制继续";
+        echo "";
+        echo -e "\033[43;31m ########################################## \033[0m";
+        echo -e "\033[43;31m #               强制忽略                 # \033[0m";
+        echo -e "\033[43;31m ########################################## \033[0m";
+        echo "";
     elif [[ $Icache == "i" || $Icache == "I" ]]; then
         for f in `find /mk8266/ -type f -name *cache`;
         do
-            echo "强制自动替换";
+            echo "";
+            echo -e "\033[43;31m ########################################## \033[0m";
+            echo -e "\033[43;31m #              强制自动替换              # \033[0m";
+            echo -e "\033[43;31m ########################################## \033[0m";
+            echo "";
             ext=${f##*.};
             cacheName=${f##*/};
             echo $cacheName;
@@ -47,7 +70,11 @@ if [[ -z $cacheF ]]; then
             done;
         done;
     else
-        echo "退出";
+        echo "";
+        echo -e "\033[43;31m ########################################## \033[0m";
+        echo -e "\033[43;31m #                 退出                   # \033[0m";
+        echo -e "\033[43;31m ########################################## \033[0m";
+        echo "";
         exit;
     fi;
 fi
@@ -57,9 +84,20 @@ WIFISSID="PS4-672_zerofo";
 WIFIPass="";
 IP="9,9,9,9";
 source /config.txt;
+echo -e "\033[43;31m ###   请检查 你的设置是否被读取生效   #### \033[0m";
+echo "";
 echo "config:"
+echo "";
+echo -e "\033[43;31m ### 如相关设置不正确请检查 config.txt #### \033[0m";
+echo "";
 cat /config.txt;
+echo "";
+echo -e "\033[43;31m ####     请核对离线包内容是否正确    ##### \033[0m";
+echo "";
 ls /mk8266;
+echo "";
+echo -e "\033[43;31m ########################################## \033[0m";
+echo "";
 cd /mk8266;
 rm .git .github -rf;
 files=(`find ./ -mindepth 1 -type f |grep -v README.md|grep -v github|grep -v cache.manifest`)
@@ -93,6 +131,24 @@ sed -i "s/.*char\*\ WIFIPass.*/\ \ \ \ \ char\*\ WIFIPass\ =\ \"${WIFIPass}\"\;/
 sed -i "s/.*IPAddress\ IP\ =\ IPAddress.*/\ \ \ \ IPAddress\ IP\ =\ IPAddress(${IP})\;/" /Ps4-wifi-http/Ps4-wifi-http.ino;
 arduino-cli compile --fqbn esp8266:esp8266:nodemcuv2:xtal=160,baud=115200,eesz=4M3M Ps4-wifi-http --output-dir=./firmware 
 mklittlefs -c /mk8266 -p 256 -b 8192 -s 0x2FA000 ./firmware/data.bin
+if [ $? -ne 0 ]; then
+    echo "";
+    echo -e "\033[43;31m ########################################## \033[0m";
+    echo -e "\033[43;31m #            离线包体积超标              # \033[0m";
+    echo -e "\033[43;31m #        请确认 confit.txt中:            # \033[0m";
+    echo -e "\033[43;31m #                                        # \033[0m";
+    echo -e "\033[43;31m #           compress=\"y\";                # \033[0m";
+    echo -e "\033[43;31m #                                        # \033[0m";
+    echo -e "\033[43;31m #           如还是不行则 去除:           # \033[0m";
+    echo -e "\033[43;31m #                                        # \033[0m";
+    echo -e "\033[43;31m #          blockArray=\"xxxxxx\"           # \033[0m";
+    echo -e "\033[43;31m #                                        # \033[0m";
+    echo -e "\033[43;31m #            此行后再试。                # \033[0m";
+    echo -e "\033[43;31m #       仍然失败则，8266无法装下此包     # \033[0m";
+    echo -e "\033[43;31m ########################################## \033[0m";
+    echo "";
+    exit;
+fi;
 srec_cat -output /output/${binname}_${packuptime}.bin -binary ./firmware/Ps4-wifi-http.ino.bin -binary  -fill 0xFF 0x0 0x00100000 ./firmware/data.bin -binary -offset 0x00100000;
 if [[ ! -f "/output/${binname}_${packuptime}.bin" ]];then
 echo "编译出错";

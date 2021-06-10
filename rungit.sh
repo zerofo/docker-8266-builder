43b4997f989dd4ccbfac4b5525fc99241d355eff
root <root@mail.zerofengt.fun>
2021-06-10 17:24:26 +0800

    add git shell

--- rungit.sh
+++ rungit.sh
@@ -2,5 +2,5 @@
 rm -rf ./tmp_data;
 cp -r $1 ./tmp_data;
 mkdir -p output;
-docker run -it --rm --name budil8266 -v $(pwd)/output/:/output -v $(pwd)/config.txt:/config.txt -v $(pwd)/tmp_data/:/mk8266  zerofo/build_ps4_8266:sd_git
+docker run -i --rm --name budil8266 -v $(pwd)/output/:/output -v $(pwd)/config.txt:/config.txt -v $(pwd)/tmp_data/:/mk8266  zerofo/build_ps4_8266:sd_git
 

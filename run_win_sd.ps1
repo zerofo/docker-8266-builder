ca946623d95620247e3ea5697883ca2b6a633dfa
Zerofo <46804648+zerofo@users.noreply.github.com>
2021-07-24 01:14:07 +0800

    Update run_win_sd.ps1

--- run_win_sd.ps1
+++ run_win_sd.ps1
@@ -1,2 +1,2 @@
 $path = Split-Path -Parent $MyInvocation.MyCommand.Definition;
-docker run -it --rm --name budil8266 -v ${path}:/output -v ${path}\config.txt:/config.txt -v ${path}\${args}:/mk8266  zerofo/build_ps4_8266:sd
+docker run -it --rm --name budil8266 -v ${path}:/output -v ${path}\config.txt:/config.txt -v ${path}\${args}:/mk8266  zerofo/build_ps4_8266:asyncsd

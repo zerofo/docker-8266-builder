403c4623e4956ba4a7d0c8c67e898afa5d3bd65b
Zerofo <46804648+zerofo@users.noreply.github.com>
2022-09-07 21:34:35 +0800

    Update mk8266.sh

--- mk8266.sh
+++ mk8266.sh
@@ -142,9 +142,9 @@ sed -i "s/.*char\*\ WIFIPass.*/\ \ \ \ \ char\*\ WIFIPass\ =\ \"${WIFIPass}\"\;/
 sed -i "s/.*IPAddress\ IP\ =\ IPAddress.*/\ \ \ \ IPAddress\ IP\ =\ IPAddress(${IP})\;/" /Ps4-wifi-http/Ps4-wifi-http.ino;
 echo "xyx===";
 arduino-cli sketch new Ps4-wifi-http
-arduino-cli compile --fqbn esp8266:esp8266:nodemcuv2:xtal=160,baud=115200,eesz=4M3M,ip=hb2f Ps4-wifi-http --output-dir=./firmware 
+arduino-cli compile --fqbn esp8266:esp8266:nodemcuv2,baud=115200,eesz=4M3M Ps4-wifi-http --output-dir=./firmware 
 echo "xyx===?";
-mklittlefs -c /mk8266 -p 256 -b 8192 -s 0x2F0000 ./firmware/data.bin
+mklittlefs -c /mk8266 -p 256 -b 8192 -s 0x2EA000 ./firmware/data.bin
 echo "xyx===2";
 if [ $? -ne 0 ]; then
     echo "";

a836f3841e807b8f84e84138e6d4e33f69475130
Zerofo <46804648+zerofo@users.noreply.github.com>
2022-09-10 15:28:19 +0800

    Update Ps4-wifi-http.ino

--- Ps4-wifi-http_async/Ps4-wifi-http.ino
+++ Ps4-wifi-http_async/Ps4-wifi-http.ino
@@ -1,4 +1,3 @@
-#include <SPI.h>
 #include <SD.h>
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
@@ -11,19 +10,24 @@ const byte PuertoDNS = 53;
 const byte PuertoHTTP = 80;
 
 bool hasSD=false;
-const int CS = D8; //SD Pinout:  D5 = CLK , D6 = MISO , D7 = MOSI , D8 = CS
-
+const int CS = D8; //SD Pinout:    D5 = CLK , D6 = MISO , D7 = MOSI , D8 = CS
+String inPage = "/index.html";
 
 struct ArchivoConfiguracion {
-  char* WIFISSID = "demo";
-  char* WIFIPass = "";
-  IPAddress IP = IPAddress(9, 9, 9, 9);
-  IPAddress Subnet = IPAddress(255, 0, 0, 0);
+    char* WIFISSID = "demo";
+    char* WIFIPass = "";
+    IPAddress IP = IPAddress(9, 9, 9, 9);
+    IPAddress Subnet = IPAddress(255, 0, 0, 0);
 };
 
 ArchivoConfiguracion Configuracion;
 AsyncWebServer WebServer(PuertoHTTP);
 DNSServer DNS;
+void ConfigurarWIFIy() {
+    WiFi.mode(WIFI_AP);
+    WiFi.softAPConfig(Configuracion.IP, Configuracion.IP, Configuracion.Subnet);
+    WiFi.softAP(Configuracion.WIFISSID);
+}
 void turnoff_usb(){
 
   // digitalWrite(D6pin, LOW);
@@ -35,129 +39,110 @@ void turnon_usb(){
   // delay(500);
   // digitalWrite(D6pin, HIGH);
 }
-void ConfigurarWIFIy() {
-  WiFi.mode(WIFI_AP);
-  WiFi.softAPConfig(Configuracion.IP, Configuracion.IP, Configuracion.Subnet);
-  if(Configuracion.WIFIPass == "")
-  WiFi.softAP(Configuracion.WIFISSID);
-  else
-  WiFi.softAP(Configuracion.WIFISSID,Configuracion.WIFIPass);
-}
 void setup() {
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  delay(0);
-  if (SD.begin(CS))
-  hasSD = true;
-  else{
-    hasSD = false;
-    LittleFS.begin();
-  }
-
-  delay(0);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, 1);
-  pinMode(D7pin, OUTPUT);
-  // pinMode(D6pin, OUTPUT);
-  digitalWrite(D7pin, LOW);
-  // digitalWrite(D6pin, LOW);
-  
-  // turnoff_usb();
-  ConfigurarWIFIy();
-  delay(0);
-
-  DNS.setTTL(300);
-  DNS.setErrorReplyCode(DNSReplyCode::ServerFailure);
-  DNS.start(PuertoDNS, "*", Configuracion.IP);
-  //Serial.println(" init. ");
-  delay(0);
-
-
-/*  WebServer.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    AsyncWebServerResponse* response = request->beginResponse(LittleFS, path, mimeType);
-    request->send(response);
-    });*/
-  WebServer.onNotFound([](AsyncWebServerRequest *request){
-    //Serial.println(" in? ");
-    if (!ManejarArchivo(request)){
-    //Serial.println(" not ");
-      //request->
-      request->redirect("/index.html");
+    if (SD.begin(CS))
+    hasSD = true;
+    else{
+        hasSD = false;
+        LittleFS.begin();
     }
-    });
-  WebServer.begin();
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, 1);
+    pinMode(D7pin, OUTPUT);
+    // pinMode(D6pin, OUTPUT);
+    digitalWrite(D7pin, LOW);
+    // turnoff_usb();
+    ConfigurarWIFIy();
+
+    DNS.setTTL(300);
+    DNS.setErrorReplyCode(DNSReplyCode::ServerFailure);
+    DNS.start(PuertoDNS, "*", Configuracion.IP);
+
+
+    WebServer.on("/", HTTP_ANY, [](AsyncWebServerRequest *request){
+    String HostIP = request->host();
+        if (!ManejarArchivo(request)){
+            request->redirect("http://"+HostIP+inPage);
+        }
+        });
+    WebServer.onNotFound([](AsyncWebServerRequest *request){
+    String HostIP = request->host();
+        if (!ManejarArchivo(request)){
+            request->redirect("http://"+HostIP+inPage);
+        }
+        });
+    WebServer.begin();
 }
 
 String obtenerTipo(String filename) {
-  if (filename.endsWith(".html")||filename.endsWith(".htm")) return "text/html";
-  else if (filename.endsWith("cache")||filename.endsWith(".manifest")) return "text/cache-manifest";
-  else if (filename.endsWith(".css")) return "text/css";
-  else if (filename.endsWith(".js")) return "application/javascript";
-  else if (filename.endsWith(".bin")) return "application/octet-stream";
-  else if (filename.endsWith(".png")) return "image/png";
-  else if (filename.endsWith(".jpg")) return "image/jpeg";
-  else if (filename.endsWith(".gz")) return "application/x-gzip";
-  else if (filename.endsWith(".svg")) return "image/svg+xml"; 
-  else if (filename.endsWith(".ttf")) return "application/x-font-ttf";
-  else if (filename.endsWith(".ico")) return "image/x-icon";
-  else if (filename.endsWith(".xml")) return "text/xml";
-  else if (filename.endsWith(".pdf")) return "application/x-pdf";
-  else if (filename.endsWith(".gif")) return "image/gif";
-  else if (filename.endsWith(".zip")) return "application/x-zip";
-  return "text/plain";
+    if (filename.endsWith(".html")||filename.endsWith(".htm")) return "text/html";
+    else if (filename.endsWith(".cache")||filename.endsWith(".manifest")) return "text/cache-manifest";
+    else if (filename.endsWith(".css")) return "text/css";
+    else if (filename.endsWith(".js")) return "application/javascript";
+    else if (filename.endsWith(".bin")) return "application/octet-stream";
+    else if (filename.endsWith(".png")) return "image/png";
+    else if (filename.endsWith(".jpg")) return "image/jpeg";
+    else if (filename.endsWith(".gz")) return "application/x-gzip";
+    else if (filename.endsWith(".svg")) return "image/svg+xml"; 
+    else if (filename.endsWith(".ttf")) return "application/x-font-ttf";
+    else if (filename.endsWith(".ico")) return "image/x-icon";
+    else if (filename.endsWith(".xml")) return "text/xml";
+    else if (filename.endsWith(".pdf")) return "application/x-pdf";
+    else if (filename.endsWith(".gif")) return "image/gif";
+    else if (filename.endsWith(".zip")) return "application/x-zip";
+    return "text/plain";
 }
 
 
 bool ManejarArchivo(AsyncWebServerRequest *request) {
-  String path = request->url();
-  if(path.endsWith("turnoff_usb")){
-    turnoff_usb();
-    Serial.println("off");
-
-    return true;
-  }
-  if(path.endsWith("turnon_usb")){
-    Serial.println("on");
-    turnon_usb();
-    return true;
-  }
-  if ((path.indexOf("/ps4/")>1) && (path.indexOf("/document/")>1))
-     path = path.substring(16,-1);  
-  if (path.endsWith("/") ) path += "index.html";
-  String mimeType = obtenerTipo(path);
-  String pathComprimido = path + ".gz";
-
-  //Serial.println(path+ " setge 1 ");
-  if(hasSD){
-    //Serial.println(path+ " setge 2 ");
-    if (SD.exists(pathComprimido) || SD.exists(path)) {
-    if (SD.exists(pathComprimido)) path += ".gz";
-    fs::File rdfile = SD.open(path,"r");
-    AsyncWebServerResponse* response = request->beginResponse(rdfile, path, mimeType);
-    if (SD.exists(pathComprimido)) 
-     response->addHeader("Content-Encoding", "gzip"); // --> uncomment if your file is GZIPPED 
-    request->send(response);
-    rdfile.close();
-    return true;
+    String path = request->url();
+    String HostIP = request->host();
+    if(path.endsWith("turnoff_usb")){
+      turnoff_usb();
+      return true;
     }
-  }
-   else{
-    if (LittleFS.exists(pathComprimido) || LittleFS.exists(path)) {
-    //Serial.println(path+ " setge 2 ");
-
-    if (LittleFS.exists(pathComprimido)) path += ".gz";
-    AsyncWebServerResponse* response = request->beginResponse(LittleFS, path, mimeType);
-    if (LittleFS.exists(pathComprimido)) 
-     response->addHeader("Content-Encoding", "gzip"); // --> uncomment if your file is GZIPPED 
-    request->send(response);
-    return true;
+    if(path.endsWith("turnon_usb")){
+      turnon_usb();
+      return true;
     }
-  }
-  return false;
+    if (path.indexOf("/document/")>1 && path.indexOf("/ps4/")> 1){
+            request->redirect("http://"+HostIP+inPage);
+        return false;
+    }
+    if (path.indexOf("?smcid=")>0){
+            request->redirect("http://"+HostIP+inPage);
+        return false;
+    }
+    if (path.endsWith("/") ) path += "index.html";
+    String mimeType = obtenerTipo(path);
+    String pathComprimido = path + ".gz";
+    if(hasSD){
+        if (SD.exists(pathComprimido) || SD.exists(path)) {
+        if (SD.exists(pathComprimido)) path += ".gz";
+        fs::File rdfile = SD.open(path,"r");
+        AsyncWebServerResponse* response = request->beginResponse(rdfile, path, mimeType);
+        if (SD.exists(pathComprimido)) 
+         response->addHeader("Content-Encoding", "gzip"); // --> uncomment if your file is GZIPPED 
+        request->send(response);
+        rdfile.close();
+        return true;
+        }
+    }
+     else{
+        if (LittleFS.exists(pathComprimido) || LittleFS.exists(path)) {
+        if (LittleFS.exists(pathComprimido)) path += ".gz";
+        AsyncWebServerResponse* response = request->beginResponse(LittleFS, path, mimeType);
+        if (LittleFS.exists(pathComprimido)) 
+         response->addHeader("Content-Encoding", "gzip"); // --> uncomment if your file is GZIPPED 
+        request->send(response);
+        return true;
+        }
+    }
+    return false;
 }
 
 void loop() {
-  delay(0);
-  
-  DNS.processNextRequest();
+    delay(10);
+    
+    DNS.processNextRequest();
 }

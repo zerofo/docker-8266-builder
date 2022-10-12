fd42e5fead7b6e0ad9c5745c8b0ca8df05370b47
Zerofo <46804648+zerofo@users.noreply.github.com>
2022-10-12 21:04:14 +0800

    Update run_win.ps1

--- run_win.ps1
+++ run_win.ps1
@@ -1,3 +1,3 @@
 $path = Split-Path -Parent $MyInvocation.MyCommand.Definition;
 echo ${path};
-docker run -it --rm --name budil8266 -v ${path}:/output -v ${path}\config.txt:/config.txt -v ` ${args}`:/mk8266  zerofo/build_ps4_8266:asyncsd
+docker run -it --rm --name budil8266 -v ${path}:/output -v ${path}\config.txt:/config.txt -v ${args}:/mk8266  zerofo/build_ps4_8266:asyncsd

6d7c3faad169f0e5e515136d7f8468ae84cbad16
Zerofo <46804648+zerofo@users.noreply.github.com>
2022-12-12 13:09:19 +0800

    Update mk32.sh

--- mk32.sh
+++ mk32.sh
@@ -116,7 +116,7 @@ do
         fi;
     fi;
 done;
-
+find ./;
 cd /;
 sed -i "s/.*char\*\ WIFISSID.*/\ \ \ \ \ char\*\ WIFISSID\ =\ \"${WIFISSID}\"\;/" /Ps4-wifi-http/Ps4-wifi-http.ino;
 sed -i "s/.*char\*\ WIFIPass.*/\ \ \ \ \ char\*\ WIFIPass\ =\ \"${WIFIPass}\"\;/" /Ps4-wifi-http/Ps4-wifi-http.ino;

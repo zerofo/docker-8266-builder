12b9837ccbc6e8b390b7ec56e01dc2d1ec760f92
Zerofo <46804648+zerofo@users.noreply.github.com>
2022-12-14 20:00:04 +0800

    Update Ps4-wifi-http.ino

--- esp32-wifi-http_async/Ps4-wifi-http.ino
+++ esp32-wifi-http_async/Ps4-wifi-http.ino
@@ -1,76 +1,112 @@
 #include <SD.h>
-//#include <ESP8266WiFi.h>
 #include <WiFi.h>
-
+#include "exfathax.h"
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
 #include <DNSServer.h>
 #include <LittleFS.h>
+// #include <LittleFS.h>
+#include <FS.h>
+#include "USB.h"
+#include <USBMSC.h>
 #define LED_BUILTIN 2
 const byte PuertoDNS = 53;
 const byte PuertoHTTP = 80;
+USBMSC dev;
 
 bool hasSD=false;
-const int CS = 17; //SD Pinout:  D5 = CLK , D6 = MISO , D7 = MOSI , D8 = CS
+String inPage = "/index.html";
 
 
 struct ArchivoConfiguracion {
-  char* WIFISSID = "demo";
+  char* WIFISSID = "Ps4_zerofo_esp32";
   char* WIFIPass = "";
   IPAddress IP = IPAddress(9, 9, 9, 9);
-  IPAddress Subnet = IPAddress(255, 0, 0, 0);
+  IPAddress Subnet = IPAddress(255, 255, 255, 0);
 };
-
+long enTime = 0;
+boolean hasEnabled = false;
 ArchivoConfiguracion Configuracion;
 AsyncWebServer WebServer(PuertoHTTP);
 DNSServer DNS;
 
 void ConfigurarWIFIy() {
   WiFi.mode(WIFI_AP);
-  WiFi.softAPConfig(Configuracion.IP, Configuracion.IP, Configuracion.Subnet);
   if(Configuracion.WIFIPass == "")
   WiFi.softAP(Configuracion.WIFISSID);
   else
   WiFi.softAP(Configuracion.WIFISSID,Configuracion.WIFIPass);
+  WiFi.softAPConfig(Configuracion.IP, Configuracion.IP, Configuracion.Subnet);
+  delay(200);
+}
+
+void turnoff_usb(){
+  enTime = 0;
+  hasEnabled = false;
+  dev.end();
+}
+void restart_esp(){
+  enTime = 0;
+  hasEnabled = false;
+  dev.end();
+  ESP.restart();
+}
+static bool onStartStop(uint8_t power_condition, bool start, bool load_eject){
+  return true;
+}
+static int32_t onRead(uint32_t lba, uint32_t offset, void* buffer, uint32_t bufsize){
+  if (lba > 4){lba = 4;}
+  memcpy(buffer, exfathax[lba] + offset, bufsize);
+  return bufsize;
+}
+// static int32_t onWrite(uint32_t lba, uint32_t offset, uint8_t* buffer, uint32_t bufsize){
+//   return bufsize;
+// }
+
+void turnon_usb(){
+  dev.vendorID("USB");
+  dev.productID("ESP32 Server");
+  dev.productRevision("3.00");
+  dev.onStartStop(onStartStop);
+  dev.onRead(onRead);
+  // dev.onWrite(onWrite);
+  dev.mediaPresent(true);
+  dev.begin(4096, 512);
+  USB.begin();
+  enTime = millis();
+  hasEnabled = true;
 }
 void setup() {
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);
-  delay(0);
-  if (SD.begin(CS))
+
+  if (SD.begin(SS))
   hasSD = true;
   else{
     hasSD = false;
     LittleFS.begin();
   }
-
-  delay(0);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, 1);
-
   ConfigurarWIFIy();
-  delay(0);
+
 
   DNS.setTTL(300);
   DNS.setErrorReplyCode(DNSReplyCode::ServerFailure);
   DNS.start(PuertoDNS, "*", Configuracion.IP);
-  //Serial.println(" init. ");
-  delay(0);
-
-
-/*  WebServer.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    AsyncWebServerResponse* response = request->beginResponse(LittleFS, path, mimeType);
-    request->send(response);
-    });*/
+  WebServer.on("/", HTTP_ANY, [](AsyncWebServerRequest *request){
+       String HostIP = request->host();
+       String path = request->url();
+        if (!ManejarArchivo(request)){
+            request->redirect("http://"+HostIP+inPage);
+        }
+  });
   WebServer.onNotFound([](AsyncWebServerRequest *request){
-    //Serial.println(" in? ");
-    if (!ManejarArchivo(request)){
-    //Serial.println(" not ");
-      //request->
-      request->redirect("/index.html");
-    }
-    });
+       String HostIP = request->host();
+       String path = request->url();
+        if (!ManejarArchivo(request)){
+            request->redirect("http://"+HostIP+inPage);
+        }
+  });
+
   WebServer.begin();
+
 }
 
 String obtenerTipo(String filename) {
@@ -95,15 +131,35 @@ String obtenerTipo(String filename) {
 
 bool ManejarArchivo(AsyncWebServerRequest *request) {
   String path = request->url();
-  if ((path.indexOf('/ps4/')>1) && (path.indexOf("/document/")>1))
-     path = path.substring(16,-1);  
-  if (path.endsWith("/") ) path += "index.html";
-  String mimeType = obtenerTipo(path);
-  String pathComprimido = path + ".gz";
+  String HostIP = request->host();
 
-  //Serial.println(path+ " setge 1 ");
+  if(path.endsWith("turnoff_usb")){
+    turnoff_usb();
+    return true;
+  }
+  if(path.endsWith("turnon_usb")){
+    turnon_usb();
+    return true;
+  }
+  if(path.endsWith("restart_esp")){
+    restart_esp();
+    return true;
+  }
+    if (path.indexOf("/document/")>1 && path.indexOf("/ps4/")> 1){
+            request->redirect("http://"+HostIP+'/jb'+inPage);
+        return false;
+    }
+    if (path.indexOf("?smcid=")>0){
+            request->redirect("http://"+HostIP+inPage);
+        return false;
+    }
+    if (path.endsWith("/") ) path += "index.html";
+
+    String mimeType = obtenerTipo(path);
+    String pathComprimido = path + ".gz";
   if(hasSD){
-    //Serial.println(path+ " setge 2 ");
+    request->send(200, "text/html", " log SD\n" );
+
     if (SD.exists(pathComprimido) || SD.exists(path)) {
     if (SD.exists(pathComprimido)) path += ".gz";
     fs::File rdfile = SD.open(path,"r");
@@ -116,22 +172,26 @@ bool ManejarArchivo(AsyncWebServerRequest *request) {
     }
   }
    else{
-    if (LittleFS.exists(pathComprimido) || LittleFS.exists(path)) {
-    //Serial.println(path+ " setge 2 ");
 
+
+
+    if (LittleFS.exists(pathComprimido) || LittleFS.exists(path)) {
     if (LittleFS.exists(pathComprimido)) path += ".gz";
     AsyncWebServerResponse* response = request->beginResponse(LittleFS, path, mimeType);
-    if (LittleFS.exists(pathComprimido)) 
-     response->addHeader("Content-Encoding", "gzip"); // --> uncomment if your file is GZIPPED 
+      if (LittleFS.exists(pathComprimido)) {
+       response->addHeader("Content-Encoding", "gzip"); // --> uncomment if your file is GZIPPED 
+
+      }
     request->send(response);
-    return true;
-    }
+    return true;}
   }
   return false;
 }
 
 void loop() {
-  delay(0);
-  
+  if (hasEnabled && millis() >= (enTime + 18000))
+   {
+    turnoff_usb();
+   } 
   DNS.processNextRequest();
 }
